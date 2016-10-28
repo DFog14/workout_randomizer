fef9bd2585f92146883465d64df0f44774558141
Dallas <dfog14@gmail.com>
2016-10-28 14:12:55 -0400

    Add additional excersices. Remove initial email tests.

--- randomizer.py
+++ randomizer.py
@@ -1,4 +1,5 @@
 import random as ran
+import time
 
 with open('abs.txt') as f:
     ab_exercises = f.readlines()
@@ -34,15 +35,14 @@ workout2.append(chest_exercises.pop(ran.randrange(len(chest_exercises))))
 workout2.append(back_exercises.pop(ran.randrange(len(back_exercises))))
 workout2.append(shoulder_exercises.pop(ran.randrange(len(shoulder_exercises))))
 
-
 workout3.append(ab_exercises.pop(ran.randrange(len(ab_exercises))))
 workout3.append(bi_exercises.pop(ran.randrange(len(bi_exercises))))
 workout3.append(leg_exercises.pop(ran.randrange(len(leg_exercises))))
 workout3.append(tri_exercises.pop(ran.randrange(len(tri_exercises))))
 workout3.append(chest_exercises.pop(ran.randrange(len(chest_exercises))))
 workout3.append(back_exercises.pop(ran.randrange(len(back_exercises))))
-workout3.append(shoulder_exercises.pop(ran.randrange(len(shoulder_exercises))))
-
+workout3.append(shoulder_exercises.pop(ran.randrange(len(shoulder_exercises))))   
+    
 temp = open('workout.txt', 'w')
 temp.write("--Workout #1--\n")
 temp.writelines(workout1)
